f14b56bc8eb95305f1106bed9e199bc8a8aaed4f
Java不良人 <1501354669@qq.com>
2022-11-24 15:42:02 +0800

    核心功能完成

---
+++ src/main/java/com/yun/entity/Comment.java
@@ -0,0 +1,20 @@
+package com.yun.entity;
+
+import lombok.Data;
+import lombok.ToString;
+
+import java.util.Date;
+
+@Data
+@ToString
+public class Comment {
+
+    private int id;
+    private int userId;
+    private int entityType;
+    private int entityId;
+    private int targetId;
+    private String content;
+    private int status;
+    private Date createTime;
+}
