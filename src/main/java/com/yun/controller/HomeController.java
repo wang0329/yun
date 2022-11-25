018f951ee3f39e7ccd00cf43a5e03635d68c7350
Java不良人 <1501354669@qq.com>
2022-11-25 10:56:30 +0800

    整合Redis   优化登录模块

--- src/main/java/com/yun/controller/HomeController.java
+++ src/main/java/com/yun/controller/HomeController.java
@@ -4,7 +4,9 @@ import com.yun.entity.DiscussPost;
 import com.yun.entity.Page;
 import com.yun.entity.User;
 import com.yun.service.DiscussPostService;
+import com.yun.service.LikeService;
 import com.yun.service.UserService;
+import com.yun.util.CommunityConstant;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -18,13 +20,16 @@ import java.util.Map;
 
 @Controller
 //首页功能
-public class HomeController {
+public class HomeController implements CommunityConstant {
 
     @Autowired
     private DiscussPostService discussPostService;
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private LikeService likeService;
 
     @RequestMapping(path = "/index", method = RequestMethod.GET)
     public String getIndexPage(Model model, Page page) {
@@ -41,6 +46,10 @@ public class HomeController {
                 map.put("post", post);
                 User user = userService.findUserById(post.getUserId());
                 map.put("user", user);
+
+                long likeCount = likeService.findEntityLikeCount(ENTITY_TYPE_POST, post.getId());
+                map.put("likeCount", likeCount);
+
                 discussPosts.add(map);
             }
         }
