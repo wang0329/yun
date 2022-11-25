8df16461b74b5d6597480eb0ba54f22521e770a3
Java不良人 <1501354669@qq.com>
2022-11-25 22:05:01 +0800

    整合Kafka   系统通知模块

--- src/main/java/com/yun/config/WebMvcConfig.java
+++ src/main/java/com/yun/config/WebMvcConfig.java
@@ -2,6 +2,7 @@ package com.yun.config;
 
 import com.yun.controller.interceptor.LoginRequiredInterceptor;
 import com.yun.controller.interceptor.LoginTicketInterceptor;
+import com.yun.controller.interceptor.MessageInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -16,6 +17,9 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Autowired
     private LoginRequiredInterceptor loginRequiredInterceptor;
 
+    @Autowired
+    private MessageInterceptor messageInterceptor;
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
 
@@ -24,6 +28,9 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 .excludePathPatterns("/**/*.css", "/**/*.js", "/**/*.png", "/**/*.jpg", "/**/*.jpeg");
 
         registry.addInterceptor(loginRequiredInterceptor)
+                .excludePathPatterns("/**/*.css", "/**/*.js", "/**/*.png", "/**/*.jpg", "/**/*.jpeg");
+
+        registry.addInterceptor(messageInterceptor)
                 .excludePathPatterns("/**/*.css", "/**/*.js", "/**/*.png", "/**/*.jpg", "/**/*.jpeg");
     }
 
--- src/main/java/com/yun/controller/FollowController.java
+++ src/main/java/com/yun/controller/FollowController.java
@@ -1,7 +1,9 @@
 package com.yun.controller;
 
+import com.yun.entity.Event;
 import com.yun.entity.Page;
 import com.yun.entity.User;
+import com.yun.event.EventProducer;
 import com.yun.service.FollowService;
 import com.yun.service.UserService;
 import com.yun.util.CommunityConstant;
@@ -30,12 +32,24 @@ public class FollowController implements CommunityConstant {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private EventProducer eventProducer;
+
     @RequestMapping(path = "/follow", method = RequestMethod.POST)
     @ResponseBody
     public String follow(int entityType, int entityId) {
         User user = hostHolder.getUser();
 
         followService.follow(user.getId(), entityType, entityId);
+
+        // 触发关注事件
+        Event event = new Event()
+                .setTopic(TOPIC_FOLLOW)
+                .setUserId(hostHolder.getUser().getId())
+                .setEntityType(entityType)
+                .setEntityId(entityId)
+                .setEntityUserId(entityId);
+        eventProducer.fireEvent(event);
 
         return CommunityUtil.getJSONString(0, "已关注!");
     }
